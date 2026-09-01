93d2ce3687db269dea03c2c00db5eacc15ebee92
yanSantos <yan.ferreira1807@gmail.com>
2026-09-01 14:27:26 -0300

    FEAT[1] - Adicionada opção de exclusão de usuário

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("=== Sistema de Cadastro ===");
         System.out.println("1 - Cadastrar usuário");
         System.out.println("2 - Listar usuários");
+        System.out.println("3 - Excluir usuário");
         System.out.println("0 - Sair");
     }
 }
